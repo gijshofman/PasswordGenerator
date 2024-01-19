ec91163ecb0952c9e6ebc48ce48723d57a7d5213
Gijs Hofman <gijs@heldertelecom.nl>
2024-01-19 21:52:08 +0100

    create docs folder

---
+++ docs/_app/immutable/chunks/navbar.MC9iSAVR.js
@@ -0,0 +1,1 @@
+import{w as a}from"./index.rCU-fHyW.js";let r=a(!1);export{r as n};
---
+++ docs/_app/immutable/chunks/paths.dlgTI5XD.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_1io5t10?.base??"",t=globalThis.__sveltekit_1io5t10?.assets??s;export{t as a,s as b};
